850ffdd5878cfc318d19457e82900ddd3aa9ed7e
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-04-09 22:02:57 +0100

    finally to initial promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
-    return ({ status: 200, body: 'success' });
-  }, () => Error()).finally(() => {
+  promise.then(() => ({ status: 200, body: 'success' }), () => Error());
+  promise.finally(() => {
     console.log('Got a response from the API');
   });
 }
