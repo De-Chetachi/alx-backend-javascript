5c7052fc53364b0da4651b3cf17d1f5dae154325
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-04-10 14:03:41 +0100

    handles error

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  uploadPhoto().then((value) => value.body)
+  return uploadPhoto().then((value) => value.body)
     .then((value) => {
       createUser().then((user) => {
         console.log(value, user.firstName, user.lastName);
       });
-    });
+    }).catch(() => console.log(Error('Signup system offline')));
 }
