781ce997a94c02ef3a77e909bdaa39fd3bbbd84c
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-04-10 13:59:56 +0100

    successively

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,1 +1,10 @@
-function handleProfileSignup()
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  uploadPhoto().then((value) => value.body)
+    .then((value) => {
+      createUser().then((user) => {
+        console.log(value, user.firstName, user.lastName);
+      });
+    });
+}
