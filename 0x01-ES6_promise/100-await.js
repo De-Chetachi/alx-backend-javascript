aebb8e45137a21d91a1f0822b9696416ea6077dd
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-04-10 23:20:20 +0100

    await

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,14 +1,12 @@
+/* eslint-disable */
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  const photo = await uploadPhoto();
-  const user = await createUser();
-  const info = await Promise.allSettled(([uploadPhoto(), createUser()]))
-    .then((responses) => responses);
-  for (const data of info) {
-    if (data.status === 'rejected') {
-      return { photo: null, user: null };
-    }
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    return { photo, user };
+  } catch (e) {
+    return { photo: null, user: null };
   }
-  return { photo, user };
 }
