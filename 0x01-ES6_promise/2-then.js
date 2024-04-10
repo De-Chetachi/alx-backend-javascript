11660119c69ca7ffc77641776bd50de2c6175061
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-04-10 13:20:06 +0100

    seperately

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({ status: 200, body: 'success' }));
-  promise.catch(() => new Error());
-  promise.finally(() => {
+  promise.then(() => {
     console.log('Got a response from the API');
+    return { status: 200, body: 'success' };
+  }, () => {
+    console.log('Got a response from the API');
+    return new Error();
   });
 }
