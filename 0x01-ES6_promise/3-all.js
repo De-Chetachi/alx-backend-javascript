c9bd4fe8a62c9691425d310fb174f439d90bc671
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-04-10 14:26:03 +0100

    all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  uploadPhoto().then((value) => value.body)
-    .then((value) => {
-      createUser().then((user) => {
-        console.log(value, user.firstName, user.lastName);
-      });
-    }).catch(() => console.log(Error('Signup system offline')));
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => console.log(Error('Signup system offline')));
 }
