42524694a3cc1142809a46d7b4ee7651d8ccf7a2
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-04-10 21:02:20 +0100

    final user

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,14 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((responses) => {
+      for (const response of responses) {
+        if (response.status === 'rejected') {
+          response.value = response.reason;
+          delete response.reason;
+        }
+      }
+    });
+}
