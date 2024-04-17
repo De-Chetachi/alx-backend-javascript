1bc27f59bf947524788353aee6e983343a9f301c
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-04-17 22:34:36 +0100

    dataview

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,12 @@
 export default function createInt8TypedArray(length, position, value) {
-  const buffer = new ArrayBuffer(length)
-  //new DataView(buffer).setInt8(position, value,);
-  const view = new Int8Array(buffer);
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+  // .setInt8(position, value,);
+  // const view = new Int8Array(buffer);
   try {
-    //return new DataView(buffer).setInt8(position, value);
-    view[position] = value;
-    return buffer;
+    // return new DataView(buffer).setInt8(position, value);
+    view.setInt8(position, value);
+    return view;
   } catch (error) {
     throw new Error('Position outside range');
   }
