87e939ca4d62524aa57689225a2e16ba72136603
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-07-23 22:09:40 +0100

    ok

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,11 @@
 export default function cleanSet(set, startString) {
   let string = '';
+
   set.forEach((element) => {
+    if (typeof startString !== "string" || typeof  element !== "string")
+    {
+      return string;
+    }
     if (element.startsWith(startString) && startString !== '') {
       // console.log(`${element}  ${string}`);
       string += element.replace(startString, '-');
