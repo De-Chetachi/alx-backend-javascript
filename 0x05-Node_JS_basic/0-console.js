fe912914610e21c6f3bbe62cdabbd6c02c5212f6
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-07-23 23:09:57 +0100

    lo

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
 module.exports = function displayMessage(message) {
-  process.stdin.write(`${message}\n`);
+  // process.stdin.write(`${message}\n`);
+  console.log(message);
 };
