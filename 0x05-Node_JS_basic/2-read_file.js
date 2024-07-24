36f79cfbaf28c1347aabade5e00236e0f0e0ab85
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-07-24 23:03:58 +0100

    io

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,11 +1,41 @@
-#!/usr/bin/env node
+const fs = require('node:fs');
 
-module.exports = function countStudents(path) {
-  //const { exec } = require(node:child_process);
-  const fs = require(node:fs)
+function countStudents(path) {
   if (!fs.existsSync(path)) {
     throw new Error('Cannot load the database');
   }
-  const page =  fs.readFileSync(path, 'utf8');
-  const line = page.split('\n')
+  const page = fs.readFileSync(path, 'utf8');
+  const lines = page.split('\n');
+  lines.shift();
+  let numStudents = 0;
+  // console.log(lines)
+  const fields = {};
+  for (const line of lines) {
+    if (line.trim() === '') continue;
+    const stu = line.split(',');
+    // if field object dont already have this field property set it
+    // stu[3] holds the field of study
+    numStudents += 1
+    if (!(stu[3] in fields)) {
+      fields[stu[3]] = { total: 1, students: [stu[0]] };
+    }
+
+    // else increment the field's total property
+    // add the students firstname to the field's students property which is a list
+    else {
+      fields[stu[3]].total += 1;
+      fields[stu[3]].students.push(stu[0]);
+    }
+  }
+  // console.log(fields);
+  // console.log(typeof(fields))
+  console.log(`Number of students: ${numStudents}`);
+  for (const field in fields) {
+    if (field) {
+      const fNames = fields[field].students.join(', ');
+      console.log(`Number of students in ${field}: ${fields[field].total}. List: ${fNames}`);
+    }
+  }
 }
+
+module.exports = countStudents;
