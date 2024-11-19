8115c689d937316a70b755dd185a166a2f5ff0d0
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-11-19 10:25:13 +0100

    ok

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
-const Utils = require('./utils');
+const Utils = require('./utils.js');
 const sinon = require('sinon')
 const sendPaymentRequestToApi = require('./3-payment.js');
 
 describe('sendPaymentRequestToApi', function() {
   it('should use Utils.calculateNumber', function() {
     const spy = sinon.spy(Utils, 'calculateNumber');
-    sendPaymentRequestApi(100, 20);
+    sendPaymentRequestToApi(100, 20);
     sinon.assert.calledWith(spy, 'SUM', 100, 20);
     spy.restore();
   });
