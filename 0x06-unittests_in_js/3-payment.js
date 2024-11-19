81ae93fbce67bbc85b15c58a1d26e82bde8eda6f
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-11-19 10:12:28 +0100

    ok

---
+++ 0x06-unittests_in_js/3-payment.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+Utils = require('./utils');
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+  const sum = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${sum}`);
+}
