ed391012ace418e0ab1f3db5d833ce4bbe77c9fa
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-11-19 20:33:43 +0100

    ok

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
 
 const chai = require('chai');
-chai.use(require("chai-as-promised"));
+//chai.use(require("chai-as-promised"));
 const expect = chai.expect;
 const getPaymentTokenFromApi = require('./6-payment_token.js');
 //const getPaymentTokenFromApi = require('./test.js');
 
 describe.only('getPaymentTokenFromApi', function() {
-  it('should return {data: Successful response from API}', function() {
-    return expect(getPaymentTokenFromApi(true)).to.eventually.deep.equal({ data: 'Successful response from the API' });
+  it('should return {data: Successful response from API}', function(done) {
+    getPaymentTokenFromApi(true).then((result) => {
+      expect(result).to.deep.equal({ data: 'Successful response from the API' });
+      console.log(result);
+      done();
+    })
   });
 });
